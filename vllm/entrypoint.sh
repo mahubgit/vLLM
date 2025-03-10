97e7ada28768865d682b191695dd219c42330d07
mahubgit <168363345+mahubgit@users.noreply.github.com>
2025-03-10 18:53:29 +0100

    tous les modeles

--- vllm/entrypoint.sh
+++ vllm/entrypoint.sh
@@ -4,6 +4,5 @@
 exec python3 -m vllm.entrypoints.openai.api_server \
     --host "0.0.0.0" \
     --port 8000 \
-    --model "/models/TinyLlama-1.1B-Chat-v1.0" \
     --trust-remote-code \
     --dtype auto
